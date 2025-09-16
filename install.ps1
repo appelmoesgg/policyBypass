e7ef2e400e48a50706959a89c143e2569bbca2db
appelmoesGG <101522959+appelmoesgg@users.noreply.github.com>
2025-09-16 12:40:47 +0200

    Add install script

---
+++ install.ps1
@@ -0,0 +1,48 @@
+# === CHECK FOR ADMIN PRIVILEGES ===
+if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole(`
+        [Security.Principal.WindowsBuiltInRole] "Administrator")) {
+
+    Write-Host "UAC is required. Restarting as administrator..."
+
+    Start-Process -FilePath "powershell.exe" `
+        -ArgumentList "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" `
+        -Verb RunAs
+
+    exit
+}
+
+# === CONFIGURATION ===
+$scriptDir = "C:\PolicyBypass"
+$batUrl = "https://raw.githubusercontent.com/appelmoesgg/policyBypass/refs/heads/main/editRegOnBoot.bat"
+$regUrl = "https://github.com/appelmoesgg/policyBypass/raw/refs/heads/main/policyBypass.reg"
+$taskUrl = "https://github.com/appelmoesgg/policyBypass/raw/refs/heads/main/policyBypass.xml"
+$batPath = Join-Path $scriptDir "editRegOnBoot.bat"
+$regPath = Join-Path $scriptDir "policyBypass.reg"
+$taskPath = Join-Path $scriptDir "policyBypass.xml"
+$taskName = "policyBypass"
+
+# === CREATE SCRIPT FOLDER IF NOT EXISTS ===
+if (-not (Test-Path $scriptDir)) {
+    New-Item -Path $scriptDir -ItemType Directory -Force
+}
+
+# === DOWNLOAD FILES ===
+Invoke-WebRequest -Uri $batUrl -OutFile $batPath -UseBasicParsing
+Invoke-WebRequest -Uri $regUrl -OutFile $regPath -UseBasicParsing
+Invoke-WebRequest -Uri $taskUrl -outFile $taskPath -UseBasicParsing
+
+# === UNBLOCK FILES ===
+Unblock-File -Path $batPath
+Unblock-File -Path $regPath
+Unblock-File -Path $taskPath
+
+# === REMOVE EXISTING TASK IF EXISTS ===
+if (Get-ScheduledTask -TaskName $taskName -ErrorAction SilentlyContinue) {
+    Unregister-ScheduledTask -TaskName $taskName -Confirm:$false
+}
+
+# === CREATE NEW SCHEDULED TASK ===
+schtasks /Create /XML $taskPath /TN $taskName
+
+Write-Host "Scheduled task '$taskName' created successfully and will run at startup."
+Read-Host -Prompt "Druk op Enter om af te sluiten"
